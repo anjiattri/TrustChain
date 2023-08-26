41107217cca44e438a5365223f865d08da583c5d
anji.attri <attri2707@gmail.com>
2023-08-26 13:34:46 +0530

    changes

--- src/components/volunteer/exisitngProgram/ExistingProgram.js
+++ src/components/volunteer/exisitngProgram/ExistingProgram.js
@@ -1,7 +1,20 @@
-import React from 'react'
-
-export default function ExistingProgram() {
+function ExistingProgram() {
+  const programs = [
+    "Program 1",
+    "Program 2",
+    "Program 3",
+    // Add more programs as needed
+  ];
   return (
-    <div>ExistingProgram</div>
-  )
+    <div>
+      {" "}
+      <ul className="program-list">
+        {programs.map((program, index) => (
+          <li key={index}>{program}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default ExistingProgram;
