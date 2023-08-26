ae06f8f4e8fd6ceed954300020c34abddb6a5b7c
Avi Gupta <avigupta@Avis-MacBook-Pro.local>
2023-08-26 14:00:02 +0530

    Added Govt changes

---
+++ src/components/govt/temp.js
@@ -0,0 +1,196 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Settings from "@mui/icons-material/Settings";
+import Analytics from "@mui/icons-material/Analytics";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Logout from "@mui/icons-material/Logout";
+import ComputerIcon from "@mui/icons-material/Computer";
+import StorageIcon from "@mui/icons-material/Storage";
+import Whatshot from "@mui/icons-material/Whatshot";
+// import EC2 from "./AWS/ec2";
+// import Database from "./AWS/database";
+// import Setting from "./AWS/settings";
+// import PageNotFound from "./AWS/pageNotFound";
+// import Firewall from "./AWS/fireWall";
+// import { Navigate, useNavigate, Route, Routes, Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function temp() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const navigateRoutes = (item) => {
+    if (item === "Ec2") {
+      navigate(`/`);
+    } else {
+      navigate(`/${item}`);
+    }
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton onClick={handleDrawerClose}>
+            <Analytics fontSize="large" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        open={open}
+        onMouseEnter={handleDrawerOpen}
+        onMouseLeave={handleDrawerClose}
+      >
+        <DrawerHeader>
+          <Typography variant="h5" alignSelf={"left"}>
+            {open == true ? <>Analytics</> : null}
+          </Typography>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Ec2", "Database", "Settings", "Firewall", "Logout"].map(
+            (text, index) => (
+              <ListItem
+                onClick={(e) => {
+                  e.preventDefault();
+                  navigateRoutes(text);
+                }}
+                key={text}
+                disablePadding
+                sx={{ display: "block" }}
+              >
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {text == "Ec2" ? (
+                      <ComputerIcon />
+                    ) : text == "Logout" ? (
+                      <Logout />
+                    ) : text == "Database" ? (
+                      <StorageIcon />
+                    ) : text == "Firewall" ? (
+                      <Whatshot />
+                    ) : text == "Settings" ? (
+                      <Settings />
+                    ) : null}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        {/* <Routes>
+          <Route path="/" element={<EC2 />} />
+          <Route exact path="/Database" element={<Database />} />
+          <Route exact path="/Settings" element={<Setting />} />
+          <Route exact path="/Firewall" element={<Firewall/>} />
+          <Route exact path="/*" element={<PageNotFound />} />
+        </Routes> */}
+      </Box>
+    </Box>
+  );
+}
